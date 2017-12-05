acdde6139a63df4e3c7b4e53576c87b2d14ff339
cakeinpanic <cakeinpanic@gmail.com>
2017-12-06 00:08:27 +0300

    move TsLinter to src folder

--- index.js
+++ index.js
@@ -1,43 +1,7 @@
 'use strict';
-const tslint = require('tslint');
-const path = require('path');
 const git = require('simple-git/promise')(process.cwd());
 const colors = require('colors');
-
-let {rulesDirectory, configurationFilename} = require('./environment');
-
-class TSLinter {
-    constructor() {
-        const options = {
-            fix: false,
-            formatter: 'json',
-            rulesDirectory: rulesDirectory
-        };
-
-        this.tsLinter = new tslint.Linter(options);
-        this.config = tslint.Configuration.loadConfigurationFromPath(path.resolve(process.cwd(), configurationFilename));
-    }
-
-    lintOneFile(filename, fileContents) {
-        this.tsLinter.lint(filename, fileContents, this.config);
-
-        return this.tsLinter.getResult().failures
-                   .map(failure => (
-                       {
-                           rule: failure.ruleName,
-                           text: failure.failure,
-                           line: failure.startPosition.lineAndCharacter.line
-                       }
-                   ));
-    }
-
-    lintFewFiles(files) {
-        return files.map(({filename, fileData}) => ({
-            filename: filename,
-            lintResult: this.lintOneFile(filename, fileData)
-        }));
-    }
-}
+const TSLinter = require('./src/tslinter');
 
 class PoliteHook {
     constructor(linter) {
---
+++ src/tslinter.js
@@ -0,0 +1,40 @@
+'use strict';
+const tslint = require('tslint');
+const path = require('path');
+
+let {rulesDirectory, configurationFilename} = require('../environment');
+
+class TSLinter {
+    constructor() {
+        const options = {
+            fix: false,
+            formatter: 'json',
+            rulesDirectory: rulesDirectory
+        };
+
+        this.tsLinter = new tslint.Linter(options);
+        this.config = tslint.Configuration.loadConfigurationFromPath(path.resolve(process.cwd(), configurationFilename));
+    }
+
+    lintOneFile(filename, fileContents) {
+        this.tsLinter.lint(filename, fileContents, this.config);
+
+        return this.tsLinter.getResult().failures
+                   .map(failure => (
+                       {
+                           rule: failure.ruleName,
+                           text: failure.failure,
+                           line: failure.startPosition.lineAndCharacter.line
+                       }
+                   ));
+    }
+
+    lintFewFiles(files) {
+        return files.map(({filename, fileData}) => ({
+            filename: filename,
+            lintResult: this.lintOneFile(filename, fileData)
+        }));
+    }
+}
+
+module.exports = TSLinter;
