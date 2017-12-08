af5206252ac81c920b24a43287bcd96b195acf72
cakeinpanic <cakeinpanic@gmail.com>
2017-12-08 14:53:38 +0300

    fix case when branch is only local

--- src/hook.js
+++ src/hook.js
@@ -1,39 +1,67 @@
 'use strict';
-const git = require('simple-git/promise')(process.cwd());
+const git = require('simple-git/promise')(process.cwd()).silent(true);
 const colors = require('colors');
+const {exec} = require('child_process');
 
 class PoliteHook {
     constructor(linter, filemask) {
-        this.tsLinter = linter;
+        this.linter = linter;
         this.filemask = filemask;
     }
 
     getAllFilesContent(filePaths) {
-        return Promise.all(filePaths.map(filename =>
-            git.show(['HEAD:' + filename])
-               .then(data => ({
-                   filename,
-                   fileData: data
-               }))
-        ));
+        return Promise
+            .all(
+                filePaths.map(filename => {
+                        // cant use git.show cause it produces fatal error
+                        return new Promise((resolve) => {
+                            exec(`git show HEAD:${filename}`,
+                                (error, data) => {
+                                    resolve({
+                                        filename,
+                                        fileData: data
+                                    });
+                                });
+                        });
+                    }
+                )
+            )
+            .then(filesData => filesData.filter(({fileData}) => !!fileData))
+    }
+
+    getOriginBranch() {
+        return git.revparse(['--abbrev-ref', 'HEAD'])
+                  .then((branchName) => {
+                      return git.revparse([`HEAD/${branchName.trim()}`])
+                                .then(() => branchName)
+                  })
+                  .catch(() => {
+                      // https://gist.github.com/joechrysler/6073741
+                      return new Promise((resolve) => {
+                          exec("git show-branch -a \\\n" +
+                              "| grep '\\*' \\\n" +
+                              "| grep -v `git rev-parse --abbrev-ref HEAD` \\\n" +
+                              "| head -n1 \\\n" +
+                              "| sed 's/.*\\[\\(.*\\)\\].*/\\1/' \\\n" +
+                              "| sed 's/[\\^~].*//'",
+                              (error, branchName) => {
+                                  resolve(branchName);
+                              });
+                      })
+                  })
+                  .then(branchName => `origin/${branchName.trim()}`)
     }
 
     getAllCommittedFilenames() {
-        return git.raw(['status', '-sb'])
-                  .then(statusReport => {
-                          let aheadCount = /ahead (\d+)/.exec(statusReport)[1];
-                          return git.revparse(['HEAD~' + aheadCount]);
-                      }
-                  )
-                  .then((lastPushedCommit) => git.diff(['HEAD', lastPushedCommit.trim(), '--name-only']))
-                  .then((info) => info.split('\n').filter(file => !!file))
-                  .catch(() => {
-                      return [];
-                  });
+        return this.getOriginBranch()
+                   .then((branchName) => git.revparse([branchName]))
+                   .then((lastPushedCommit) => git.diff(['HEAD', lastPushedCommit.trim(), '--name-only']))
+                   .then((info) => info.split('\n'))
+                   .catch(() => []);
+
     }
 
     outputErrors(lintResults) {
-
         if (lintResults.length) {
             console.log(colors.blue(`I have linted files by mask ${this.filemask} committed since last push and there are lint errors:`));
         } else {
@@ -55,9 +83,7 @@ class PoliteHook {
 
     lintCommitted() {
         this.getAllCommittedFilenames()
-            .then(filenames => {
-                return filenames.filter(filename => this.filemask.test(filename))
-            })
+            .then(filenames => filenames.filter(filename => this.filemask.test(filename)))
             .then(filenames => {
                 if (!filenames.length) {
                     console.log(colors.green(`No files to lint (mask ${this.filemask})`));
@@ -65,7 +91,7 @@ class PoliteHook {
                 }
                 return this.getAllFilesContent(filenames);
             })
-            .then((files) => this.tsLinter.lintFewFiles(files))
+            .then((files) => this.linter.lintFewFiles(files))
             .then(lintData => lintData.filter(({lintResult}) => lintResult.length))
             .then(lintData => this.outputErrors(lintData))
             .catch(err => {
